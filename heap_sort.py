4f993bd11c57f34ca84467081d1011e3ffa696d9
shw210 <72764827+shw210@users.noreply.github.com>
2020-10-19 16:39:27 -0400

    Add files via upload

---
+++ heap_sort.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 19 14:49:57 2020
+
+@author: a693658
+"""
+
+def heapify(A, n, i):
+    
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+    
+    if (left < n and A[left] > A[largest]):
+        largest = left
+    if (right < n and A[right] > A[largest]):
+        largest = right
+        
+    if largest != i:
+        A[largest], A[i] = A[i], A[largest]        
+        heapify(A, n, largest)
+        
+def heap_sort(A):
+    n = len(A)
+    # build max_heap using heapify function
+    for i in range(n//2 - 1, -1, -1):
+        heapify(A, n, i)
+        
+    # sort by keep removing the first element of the max_heap
+    for i in range(n-1, 0, -1):
+        A[0], A[i] = A[i], A[0]
+        heapify(A, i, 0)
+    
+A = [ 3, 1, 7, 3, -4, 28, 9]        
+heap_sort(A)
