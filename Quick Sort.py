32b523d799ec64acf2cb97f091b147bd9bc94dbf
shw210 <72764827+shw210@users.noreply.github.com>
2020-10-28 16:19:26 -0400

    Update Quick Sort.py

--- Quick Sort.py
+++ Quick Sort.py
@@ -1,38 +1,23 @@
-def partition(array, start, end):
-    pivot = array[start]
-    low = start + 1
-    high = end
-
-    while True:
-        # If the current value we're looking at is larger than the pivot
-        # it's in the right place (right side of pivot) and we can move left,
-        # to the next element.
-        # We also need to make sure we haven't surpassed the low pointer, since that
-        # indicates we have already moved all the elements to their correct side of the pivot
-        while low <= high and array[high] >= pivot:
-            high = high - 1
-
-        # Opposite process of the one above
-        while low <= high and array[low] <= pivot:
-            low = low + 1
-
-        # We either found a value for both high and low that is out of order
-        # or low is higher than high, in which case we exit the loop
-        if low <= high:
-            array[low], array[high] = array[high], array[low]
-            # The loop continues
-        else:
-            # We exit out of the loop
-            break
-
-    array[start], array[high] = array[high], array[start]
-
-    return high
+def partition(self, nums, start, end, pivot_index):
+        pivot = nums[pivot_index]
+        
+        nums[end], nums[pivot_index] = nums[pivot_index], nums[end]
+        
+        next_small_index = start
+        
+        for i in range(start, end):
+            if nums[i] < pivot:
+                nums[i], nums[next_small_index] = nums[next_small_index], nums[i]
+                next_small_index += 1
+                
+        nums[next_small_index], nums[end] = nums[end], nums[next_small_index]
+        
+        return next_small_index
     
 def quick_sort(array, start, end):
     if start >= end:
         return
-
-    p = partition(array, start, end)
+    pivot_index = random.randint(start, end)
+    p = partition(array, start, end, pivot_index)
     quick_sort(array, start, p-1)
     quick_sort(array, p+1, end)
